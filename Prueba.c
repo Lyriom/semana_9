0a1ab767ffd78acdcbef36ee3f2cb2ff1e264d24
josue riera <josueriera23@hotmail.com>
2023-05-22 16:00:17 -0500

    segunda version

--- Prueba.c
+++ Prueba.c
@@ -11,3 +11,13 @@ int main() {
     
     printf("Ingrese el número de columnas de la matriz: ");
     scanf("%d", &columnas);
+     // Crear la matriz
+    int matriz[filas][columnas];
+    
+    // Llenar la matriz con números aleatorios entre 0 y 100
+    srand(time(NULL));
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = rand() % 101; // Números aleatorios entre 0 y 100
+        }
+    }
