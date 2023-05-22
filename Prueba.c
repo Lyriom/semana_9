8720ff912792aca9964c75f095871e84669cea65
josue riera <josueriera23@hotmail.com>
2023-05-22 15:52:57 -0500

    version 1

---
+++ Prueba.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int main() {
+    int filas, columnas;
+    
+    printf("Ingrese el número de filas de la matriz: ");
+    scanf("%d", &filas);
+    
+    printf("Ingrese el número de columnas de la matriz: ");
+    scanf("%d", &columnas);
